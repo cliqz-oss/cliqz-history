057ecfdf83462953399dd21c012e20ae9a223340
Vladimir Zhuravlev <vladimir@cliqz.com>
2017-09-04 13:51:34 +0200

    use extension locale

---
+++ app/instance-initializers/i18n.js
@@ -0,0 +1,11 @@
+export function initialize(appInstance) {
+  const i18n = appInstance.lookup('service:i18n');
+  const lang = typeof chrome !== 'object' ? 'en' :
+    chrome.i18n.getMessage('lang_code');
+  i18n.set('locale', lang);
+}
+
+export default {
+  name: 'i18n',
+  initialize
+};
