d8313cac6c5efb06d05ca60dd2a81dc15e639879
Vladimir Zhuravlev <private.face@gmail.com>
2017-12-05 11:15:34 +0100

    EX-6241: support english locale (#12)
    * EX-6241: support english locale
    
    * EX-6241: wait for chrome object
    
    * EX-6241: move i18n and moment initialization to app router

---
+++ app/application/route.js
@@ -0,0 +1,29 @@
+/* global chrome */
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+import Ember from "ember";
+
+function waitForChrome(callback) {
+  if (typeof chrome !== 'object') {
+    setTimeout(() => waitForChrome(callback), 50);
+  } else {
+    callback(chrome);
+  }
+}
+
+export default Ember.Route.extend({
+  moment: Ember.inject.service('moment'),
+  i18n: Ember.inject.service('i18n'),
+  beforeModel() {
+    return new Ember.RSVP.Promise((resolve) => {
+      waitForChrome((chrome) => {
+        const i18n = this.get('i18n');
+        const moment = this.get('moment');
+        const lang = chrome.i18n.getMessage('lang_code') || 'en';
+        i18n.set('locale', lang);
+        moment.setLocale(lang);
+        resolve();
+      });
+    });
+  }
+});
+
--- config/environment.js
+++ config/environment.js
@@ -26,7 +26,7 @@ module.exports = function(environment) {
     moment: {
       // To cherry-pick specific locale support into your application.
       // Full list of locales: https://github.com/moment/moment/tree/2.10.3/locale
-      includeLocales: ['de', 'fr']
+      includeLocales: ['en', 'de', 'fr']
     }
   };
 
