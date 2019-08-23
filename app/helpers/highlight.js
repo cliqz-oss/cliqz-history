e9b0e18ff2c9d3b17600bc2c710bd20e6edc3255
Vladimir Zhuravlev <vladimir@cliqz.com>
2019-08-23 11:28:38 +0200

    EX-9243: Escape query before converting it to regexp

--- app/helpers/highlight.js
+++ app/helpers/highlight.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
+function toRegexp(text) {
+  const escapedText = text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  return new RegExp(escapedText, 'gi');
+}
+
 export function highlight([text, em]) {
   if (!em) {
     return text;
   }
 
   var safeText = Ember.Handlebars.Utils.escapeExpression(text || '');
-  const re = new RegExp(em, 'gi');
+  const re = toRegexp(em);
   const highlighted = safeText.replace(re, function (match) {
     return `<em>${match}</em>`;
   });
